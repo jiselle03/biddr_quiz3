c90f4390ac80d29d394dfaa276d6a937cc05d5c8
Jiselle Liu <jiselle03@gmail.com>
2020-02-18 13:03:10 -0800

    Generate seed file

--- biddr_api/db/seeds.rb
+++ biddr_api/db/seeds.rb
@@ -1,7 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Bid.delete_all
+Auction.delete_all
+User.delete_all
+
+PASSWORD = "supersecret";
+
+super_user = User.create( 
+    first_name: "Padme", 
+    last_name: "Amidala", 
+    email: "padme@naboo.gov", 
+    password: PASSWORD,
+    is_admin: true
+) 
+
+20.times do 
+    full_name = Faker::Movies::StarWars.character.split(' ')
+    first_name = full_name[0]
+    last_name = full_name[1]
+    User.create( 
+        first_name: first_name, 
+        last_name: last_name,  
+        email: "#{first_name}.#{last_name}@#{Faker::Movies::StarWars.planet}.com", 
+        password: PASSWORD 
+    )  
+end 
+
+users = User.all 
+puts Cowsay.say("Created #{users.count} users.", :dragon)  
+puts "Login with #{super_user.email} and password of '#{PASSWORD}'."
+
+100.times do
+    random_date = Faker::Date.backward(days:365 * 5)
+    a = Auction.create(
+        title: Faker::Commerce.product_name,
+        description: Faker::Movies::StarWars.quote,
+        ends_at: Faker::Date.forward(days: 60),
+        reserve_price: Faker::Number.within(range: 1..10000),
+        created_at: random_date,
+        updated_at: random_date,
+        user: users.sample
+    )
+    if a.valid?
+        a.bids = rand(0..15).times.map do
+            Bid.new(
+                price: Faker::Number.within(range: 1..10000),
+                created_at: random_date,
+                updated_at: random_date,
+                user: users.sample
+            )
+        end
+    end
+end
+
+puts Cowsay.say("Generated #{Auction.count} auctions.", :frogs)
+puts Cowsay.say("Generated #{Bid.count} bids.", :frogs)
